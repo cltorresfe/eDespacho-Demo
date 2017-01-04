88d2847a8ff91feb900da15054323964e06f11f9
Claudio Torres <cltorresfe@gmail.com.com>
2017-01-04 18:26:29 -0400

    adding warehouse for user on softland database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,17 +17,37 @@ class UsersController < ApplicationController
     end
 
     def update
+        @store = Store.find(params[:user][:warehouse_id])
+        @warehouse = Warehouse.find_by(id: params[:user][:warehouse_id])
+        unless @warehouse.present?
+          @warehouse = Warehouse.new
+          @warehouse.id = @store.CodBode
+          @warehouse.name = @store.DesBode
+          @warehouse.address = @store.Direc
+          @warehouse.save!
+        end
         @user = User.find(params[:id])
         if @user.update_attributes(user_params)
-            redirect_to root_path, success: "Usuario actualizado satisfactoriamente."
+          bypass_sign_in(@user)
+          redirect_to root_path, success: "Usuario actualizado satisfactoriamente."
         else
             render :edit, danger: "Ha ocurrido un error al ingresar los datos." 
         end
     end
 
     def create
+      @store = Store.find(params[:user][:warehouse_id])
+      @warehouse = Warehouse.find_by(id: params[:user][:warehouse_id])
+      unless @warehouse.present?
+        @warehouse = Warehouse.new
+        @warehouse.id = @store.CodBode
+        @warehouse.name = @store.DesBode
+        @warehouse.address = @store.Direc
+        @warehouse.save!
+      end
       @user = User.new(user_params)
       if @user.save
+        bypass_sign_in(current_user)
         redirect_to root_path, success: "Usuario Ingresado Satisfactoriamente." 
       else
         render :new, danger: "Ha ocurrido un error al ingresar los datos." 
