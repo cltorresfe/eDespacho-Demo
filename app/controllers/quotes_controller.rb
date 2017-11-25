eff1ec4d9b910d42920c06754ba46dfc5e61782a
Claudio Torres <cltorresfe@gmail.com.com>
2017-11-25 18:02:13 -0300

    elimina cotizacion

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -13,12 +13,33 @@ class QuotesController < ApplicationController
       @customer_quote.total_quote = @cotizaciones.sum(:total_price) if @cotizaciones.present?
       @customer_quote.save!
     end
-
+    flash.now[:warning] = "No se encontraron resultados para su búsqueda" unless @cotizaciones.present?
   	respond_to do |format|
   	  format.html
   	  format.pdf { render template: 'quotes/carro_cotizacion', pdf: 'Cotizacion Cliente' }
   	  format.xls # { send_data @prueba.to_csv(col_sep: "\t") }
   	end
+  end
+  def destroy
+    @quote = Quote.find(params[:id])
+    @quote.destroy
+    @cotizaciones = Quote.where(user: current_user)
+    respond_to do |format|
+      format.html { redirect_to carro_cotizacion_path }
+    end
+  end
+  def edit
+    @quote = Quote.find(params[:id])
+  end
+  def update
+    @quote = Quote.find(params[:id])
+    respond_to do |format|
+      if @quote.update_attributes(params[:quote])
+        format.html { redirect_to carro_cotizacion_path, notice: 'Quote was successfully updated.' }
+      else
+        format.html { render action: "edit" }
+      end
+    end
   end
 
 end
